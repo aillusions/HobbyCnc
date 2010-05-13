e162ecb96716843e21fc0aab0b0340ae12824102
Oleksandr Zalizniak <aillusions@gmail.com>
2010-05-13 22:02:22 +0200

    minor changes

--- GCodeEditor/src/cnc/editor/Editor.java
+++ GCodeEditor/src/cnc/editor/Editor.java
@@ -1,13 +1,10 @@
 package cnc.editor;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 
-import javax.swing.JComboBox;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.Document;
 import javax.swing.text.PlainDocument;
@@ -70,17 +67,14 @@ public class Editor implements GCodeAcceptor{
 	}
 	
 	public void scale(float ratio) {
-		//scale = Float.parseFloat(comBox_Scale.getSelectedItem().toString());
-		//pnl_GraphicOutput.setScale(ratio);
-		//pnl_GraphicOutput.repaint();
+
 	}
 	
 	protected void clear() {
 		try {
 			doc.remove(0, doc.getLength());
 		} catch (BadLocationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw new RuntimeException(e);
 		}
 	}
 
--- GCodeEditor/src/cnc/editor/Main.java
+++ GCodeEditor/src/cnc/editor/Main.java
@@ -5,9 +5,9 @@ import cnc.editor.view.EditorViewFrame;
 public class Main {	
 	
 	public static void main(String[] args) {		
-		Editor editor = new Editor();			
+		Editor editor = new Editor();
 		EditorViewFrameListener viewListener = new EditorViewFrameListener(editor);		
-		EditorViewFrame editorViewFrame = new EditorViewFrame(viewListener, editor.getDoc());			
+		EditorViewFrame editorViewFrame = new EditorViewFrame(viewListener, editor.getDoc());
 		GCodesDocListener docListener = new GCodesDocListener(editorViewFrame);			
 		editor.getDoc().addDocumentListener(docListener);
 	}
